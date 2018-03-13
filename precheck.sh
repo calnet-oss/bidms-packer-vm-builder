59933f74486311b57402b19f4629d613704050b2
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2018-03-13 09:54:33 -0700

    Upgrade to Debian 9.4

--- precheck.sh
+++ precheck.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-if [ ! -e debian/debian-9.3.0-amd64-xfce-CD-1.iso ]; then
-  echo "Missing: debian/debian-9.3.0-amd64-xfce-CD-1.iso"
+DEBIAN_VERSION=$(cat debian/debianVersion.txt)
+
+if [ ! -e debian/debian-${DEBIAN_VERSION}-amd64-xfce-CD-1.iso ]; then
+  echo "Missing: debian/debian-${DEBIAN_VERSION}-amd64-xfce-CD-1.iso"
   echo "Download with debian/downloadDebian.sh and verify with"
   echo "debian/verifyDebian.sh"
   exit 1
